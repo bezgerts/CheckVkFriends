85ecd601d8ff24c45634dae8174074e8584eadb9
Arkadiy Bezgerc <arkadiy.bezgerc@ocrv.ru>
2019-05-30 18:47:32 +0300

    working on oauth2-vk

--- src/main/java/me/bezgerts/VkFriendsChecker/VkFriendsCheckerApplication.java
+++ src/main/java/me/bezgerts/VkFriendsChecker/VkFriendsCheckerApplication.java
@@ -2,12 +2,16 @@ package me.bezgerts.VkFriendsChecker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @SpringBootApplication
+
 public class VkFriendsCheckerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VkFriendsCheckerApplication.class, args);
 	}
+
 
 }
