16f529e45429cd24856c9c6cd7b5bbd6886dfb62
Arkadiy Bezgerc <arkadiy.bezgerc@ocrv.ru>
2019-05-30 17:19:03 +0300

    Initial commit

---
+++ src/main/java/me/bezgerts/VkFriendsChecker/VkFriendsCheckerApplication.java
@@ -0,0 +1,13 @@
+package me.bezgerts.VkFriendsChecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkFriendsCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkFriendsCheckerApplication.class, args);
+	}
+
+}
